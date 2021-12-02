0d8688ffd4eb03fbfb603317910e772ea55e8aca
pascal.edouard@brambles.com <pascal@edouard.tech>
2021-12-02 12:24:42 +0000

    cleanup

--- lambda.tf
+++ lambda.tf
@@ -5,9 +5,6 @@ resource "aws_lambda_function" "pet_lambda" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "${var.pet_script_filename}.pet_info"
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("./files/lambda_function_payload.zip"))}"
   source_code_hash = data.archive_file.pet_script.output_base64sha256
 
   runtime = "python3.8"
